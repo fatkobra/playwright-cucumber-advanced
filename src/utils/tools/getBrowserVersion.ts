ce11988e6c61d37eea2cecb9cb3ad2400d4fa548
fatkobra <55045047+fatkobra@users.noreply.github.com>
2025-08-04 11:10:40 +0200

    Update getBrowserVersion.ts

--- src/utils/tools/getBrowserVersion.ts
+++ src/utils/tools/getBrowserVersion.ts
@@ -1,6 +1,10 @@
 import { Page } from '@playwright/test';
 
 export async function getBrowserVersion(page: Page): Promise<string> {
-  const version = await page.context().browser().version();
-  return version;
+  const browser = page.context().browser();
+  if (!browser) {
+    throw new Error('Unable to retrieve browser instance from context.');
+  }
+
+  return browser.version();
 }
